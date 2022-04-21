db070e7caa7c834b46e5679f5ff686dfa1c35a01
Weston <barnesweston@gmail.com>
2022-04-21 17:18:41 -0500

    add queries for delete, add, and get a candidate

--- server.js
+++ server.js
@@ -23,10 +23,37 @@ app.get('/', (req,res) => {
     });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
+// get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,res) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(res);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, res) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(res);
+// });
 
 // default response for any other request (NOT FOUND)
 app.use((req,res) => {
