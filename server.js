acc48f91e4978b9b69f384de6c4f0ad2672a0383
Weston <barnesweston@gmail.com>
2022-04-21 20:18:02 -0500

    add routes for party CRUD

--- server.js
+++ server.js
@@ -59,6 +59,50 @@ app.get('/api/candidate/:id', (req,res) => {
     });
 });
 
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.statusMessage(500).json({ error: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/parties/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({message: 'Party not found.'});
+        } else {
+            res.json ({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // delete a candidate
 app.delete('/api/candidate/:id', (req,res) => {
